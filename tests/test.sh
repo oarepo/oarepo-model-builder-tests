4a05eb4703269dc2c132c0ef028e3684f9268593
Ronald Krist <ronald.krist@cesnet.cz>
2022-10-20 12:41:18 +0200

    added test dependencies in oarepo model builder setup.cfg patch

--- tests/test.sh
+++ tests/test.sh
@@ -30,15 +30,6 @@ cd $DIR
 python3 -m venv venv
 . venv/bin/activate
 pip install -e ".[tests]"
-pip install "invenio-app>=1.3.3"
-pip install "invenio-db[postgresql,mysql,versioning]>=1.0.14,<2.0.0"
-pip install "pytest-invenio>=1.4.11"
-pip install "invenio_search[elasticsearch7]>=2.0.0"
-    #Sphinx>=4.5
-    #openpyxl>=3.0.0<4.0.0
-pip install "Werkzeug<2.2.0"
-pip install "Flask-Login>=0.6.1"
-pip install "pyyaml>=6.0"
 python tests/conftest.py
 python tests/test_resource.py
 python tests/test_service.py
