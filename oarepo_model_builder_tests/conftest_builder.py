d014f2434cdbd116fec6124dad6ec4f0a59ebc7c
Mirek Simek <miroslav.simek@gmail.com>
2023-05-08 16:14:07 +0200

    working on structured schema

--- oarepo_model_builder_tests/conftest_builder.py
+++ oarepo_model_builder_tests/conftest_builder.py
@@ -4,13 +4,6 @@ from oarepo_model_builder.invenio.invenio_base import InvenioBaseClassPythonBuil
 class ConftestBuilder(InvenioBaseClassPythonBuilder):
     TYPE = "invenio_tests_conftest"
     template = "conftest"
-    MODULE = "tests.conftest"
 
-    def finish(self, **extra_kwargs):
-        python_path = self.module_to_path(self.MODULE)
-        self.process_template(
-            python_path,
-            self.template,
-            schema=self.schema,
-            **extra_kwargs,
-        )
+    def _get_output_module(self):
+        return f'{self.current_model.definition["tests"]["module"]}.conftest'
--- oarepo_model_builder_tests/utils_builder.py
+++ oarepo_model_builder_tests/utils_builder.py
@@ -4,13 +4,6 @@ from oarepo_model_builder.invenio.invenio_base import InvenioBaseClassPythonBuil
 class TestUtilsBuilder(InvenioBaseClassPythonBuilder):
     TYPE = "test_utils"
     template = "test_utils"
-    MODULE = "tests.utils"
 
-    def finish(self, **extra_kwargs):
-        python_path = self.module_to_path(self.MODULE)
-        self.process_template(
-            python_path,
-            self.template,
-            schema=self.schema,
-            **extra_kwargs,
-        )
+    def _get_output_module(self):
+        return f'{self.current_model.definition["tests"]["module"]}.utils'
