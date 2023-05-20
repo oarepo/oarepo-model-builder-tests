02fcf7535bfc6cc438b7863f1550682c03cc1630
Mirek Simek <miroslav.simek@gmail.com>
2023-05-20 08:37:15 +0200

    Working on 4.0 release

--- oarepo_model_builder_tests/datatypes/components.py
+++ oarepo_model_builder_tests/datatypes/components.py
@@ -11,22 +11,21 @@ class TestSchema(ma.Schema):
         data_key="extra-fixtures",
         attribute="extra-fixtures",
         required=False,
-        load_default=[]
+        load_default=[],
     )
 
     extra_code = ma.fields.String(
-        data_key="extra-code",
-        attribute="extra-code",
-        load_default=""
+        data_key="extra-code", attribute="extra-code", load_default=""
     )
 
-    module = ma.fields.String(load_default='tests')
+    module = ma.fields.String(load_default="tests")
+
 
 class ModelTestComponent(DataTypeComponent):
     eligible_datatypes = [ModelDataType]
 
     class ModelSchema(ma.Schema):
-        tests = ma.fields.Nested(TestSchema, load_default=TestSchema().load({}))
+        tests = ma.fields.Nested(TestSchema, load_default=lambda: TestSchema().load({}))
 
 
 components = [ModelTestComponent]
